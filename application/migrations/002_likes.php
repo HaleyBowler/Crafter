280b44d80f92f9b7556146c0640f3ab057e61ff3
marksteinbrick <marksteinbrick@collage.harvard.edu>
2015-07-21 12:16:18 +0200

    We in dat database

---
+++ application/migrations/002_likes.php
@@ -0,0 +1,44 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Migration_Likes extends CI_Migration {
+
+            public function up()
+            {
+                    $this->dbforge->add_field(array(
+                            'id' => array(
+                                    'type' => 'INT',
+                                    'constraint' => 5,
+                                    'unsigned' => TRUE,
+                                    'auto_increment' => TRUE
+                            ),
+                            'user_id' => array(
+                                    'type' => 'VARCHAR',
+                                    'constraint' => '50'
+                            ),
+                            'picture_url' => array(
+                                    'type' => 'VARCHAR',
+                                    'constraint' => '50'
+                            ),
+                            'project_url' => array(
+                                    'type' => 'VARCHAR',
+                                    'constraint' => '50'
+                            ),
+                            'description' => array(
+                                    'type' => 'VARCHAR',
+                                    'constraint' => '255'
+                            )
+
+                    ));
+                    $this->dbforge->add_key('id', TRUE);
+                    $this->dbforge->create_table('likes');
+            }
+
+            public function down()
+            {
+                    $this->dbforge->drop_table('likes');
+            }
+    }
+
+?>
