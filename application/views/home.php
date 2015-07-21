7455492dbcfcb698383e573813f7d1dc2d1d6ae5
HaleyBowler <hjb2dq@virginia.edu>
2015-07-21 11:22:28 +0200

    Pinterest API

--- application/views/home.php
+++ application/views/home.php
@@ -11,6 +11,19 @@
 		<?php echo "Welcome ".$this->session->userdata('username');?>
 	</div>
 	<div>
+		<?php
+		$jsonurl = "http://api.pinterest.com/v3/pidgets/users/pagingfunmums/pins/";
+		$json = file_get_contents($jsonurl);
+		$jfo = json_decode($json);
+		$pins = $jfo->data->pins;
+		foreach ($pins as $pin) {
+			$img = $pin->images;
+			$array = json_decode(json_encode($img), true);
+			$imgURL = $array["237x"]["url"];
+			$image = file_get_contents($imgURL);
+			file_put_contents('/assets/file.jpg', $image); //Where to save the image on your server
+		}
+		?>
 	</div>
 </body>
 </html>
